7271b641e575ffd41ece400e1c7ccb32522ff367
Thore Weilbier <thore@weilbier.net>
2018-10-21 00:53:59 +0200

    Several minor improvements.
    Export the format string structuring into extra function for initState
    and formatState as single definition.
    Add new icon as separator between message and account count.
    Change the default account icon.
    Remove the color configurations due to no usage.

--- notmuch.sh
+++ notmuch.sh
@@ -4,6 +4,12 @@
 # This segment displays the current mail status by notmuch.
 
 
+# Format the given information into the string with icons.
+#
+function formatState {
+  echo "${NOTMUCH_ICON_MESSAGES} $1 ${NOTMUCH_ICON_SEPARATOR} ${NOTMUCH_ICON_ACCOUNTS} $2"
+}
+
 # Interface
 
 # Implement the interface function for the initial subscription state.
@@ -11,7 +17,7 @@
 function initState_notmuch {
   # Trigger hook for first time to get current state.
   $(dirname ${BASH_SOURCE[0]})/notmuch_hook.py &
-  STATE="${NOTMUCH_ICON_MESSAGES} 0 ${NOTMUCH_ICON_ACCOUNTS} 0"
+  STATE="$(formatState 0 0)"
 }
 
 # Implement the interface function to format the current state of the subscription.
@@ -20,5 +26,6 @@ function format_notmuch {
   IFS=' ' read -ra numbers <<< "$1"
   messages="${numbers[0]}"
   accounts="${numbers[1]}"
-  STATE="${NOTMUCH_ICON_MESSAGES} ${messages} ${NOTMUCH_ICON_ACCOUNTS} ${accounts}"
+  eval "state=\$(formatState ${messages} ${accounts})"
+  STATE="$state"
 }
